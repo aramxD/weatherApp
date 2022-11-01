53c239ae8bcd249fdfac0613c730d8e7596513b1
julio santacruz <aram.jsh1@gmail.com>
2022-11-01 00:16:51 -0700

    fetchData Ready

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import WeatherCard from "../elements/WeatherCard";
+ 
 
-function WeatherApp() {
+function WeatherApp({ className }) {
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.weather.gov/gridpoints/SGX/59,19/forecast?units=si"
+      );
+      const data = await response.json();
+      setWeatherData(data.properties.periods);
+    }
+    fetchData();
+  }, []);
+
+  //console.log(weatherData);
+
   return (
-    <div>Soy WeatherApp</div>
-  )
+    <section className={className}>
+      <h1>Weather App</h1>
+      <div className="cityData">
+        
+        <p>City: Sandiego</p>
+        <p>State: CA</p>
+      </div>
+      
+      {weatherData?.map((day)=>{
+        return(<WeatherCard key={day.number} day={day}/>)
+      })}
+    </section>
+  );
 }
 
-export default WeatherApp
+export default styled(WeatherApp)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  .cityData {
+    display: flex;
+    justify-content: space-evenly;
+    width: 80%;
+  }
+`;
---
+++ src/elements/WeatherCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+
+function WeatherCard({className, day}){
+
+    return(
+        <article>
+            
+        <p>Day: {day.name}  {day.isDaytime?'☀️':'🌜'}</p>
+        <p>Date: {day.startTime.substring(5,10)}</p>
+        <p>Temperature: {day.temperature} C</p>
+        <p>Wind Speed: {`${day.windSpeed} [ ↗️ ${day.windDirection} ]`}</p>
+        <p>Forecast: {day.shortForecast}</p>
+      </article>
+    )
+}
+
+export default styled(WeatherCard)`
+    
+`
